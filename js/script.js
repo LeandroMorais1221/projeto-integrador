1e0a0e3b7498772658ee60b9b411fb9acab802fb
LeandroMorais1221 <Leandromorais1221@gmail.com>
2024-09-11 12:21:36 -0300

    Realizado Ajustes na Responsividade
    Realizado Ajustes no footer, section produtos e alguns cards

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let menuHamburguer = document.querySelector('#menu-mobile');
+let navegacaoMobile = document.querySelector('#navegacao-mobile');
+
+menuHamburguer.addEventListener('click', (e)=>{
+    e.preventDefault();
+    abrirNavegacaoMobile();
+
+
+})
+
+function abrirNavegacaoMobile(){
+    if(navegacaoMobile.style.display == 'block'){
+        navegacaoMobile.style.display = 'none';
+        menuHamburguer.innerHTML = '<i class="fa-solid fa-bars" style="color: #ffffff;"></i>'
+        document.body.style.position = "initial";
+    }
+    else{
+        navegacaoMobile.style.display = 'block';
+        menuHamburguer.innerHTML = '<i class="fa-solid fa-x" style="color: #ffffff;"></i>'
+        document.body.style.position = "fixed";
+    
+    }
+}
+
