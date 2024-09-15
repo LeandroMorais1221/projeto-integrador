0e8b1c27671b34312559392ce4bf2aaddc58041d
LeandroMorais1221 <Leandromorais1221@gmail.com>
2024-09-15 17:07:18 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 let menuHamburguer = document.querySelector('#menu-mobile');
 let navegacaoMobile = document.querySelector('#navegacao-mobile');
 
-menuHamburguer.addEventListener('click', (e)=>{
-    e.preventDefault();
-    abrirNavegacaoMobile();
 
-
-})
-
-function abrirNavegacaoMobile(){
+menuHamburguer.addEventListener('click', ()=>{
     if(navegacaoMobile.style.display == 'block'){
         navegacaoMobile.style.display = 'none';
         menuHamburguer.innerHTML = '<i class="fa-solid fa-bars" style="color: #ffffff;"></i>'
@@ -18,7 +12,6 @@ function abrirNavegacaoMobile(){
         navegacaoMobile.style.display = 'block';
         menuHamburguer.innerHTML = '<i class="fa-solid fa-x" style="color: #ffffff;"></i>'
         document.body.style.position = "fixed";
-    
     }
 }
-
+);
